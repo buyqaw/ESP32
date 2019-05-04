a02b25f7f05024af068b568518cdc3c7169396b9
BiggyBaron <dr.cleverest@gmail.com>
2019-05-04 19:59:33 +0600

    Working test BuyNode

--- BuyNodeBIClients/src/main.cpp
+++ BuyNodeBIClients/src/main.cpp
@@ -8,29 +8,29 @@ static BLEUUID    serviceUUID("ba10");
 
 // The characteristic of the remote service we are interested in.
 static BLEUUID    checkUUID("ba52");
-static BLEUUID    passwdUUID("ba44");
-static BLEUUID    adminUUID("ba13");
-static BLEUUID    actionUUID("ba87");
-static BLEUUID    incomingUUID("ba76");
 
 static BLEClient*  pClient;
 
+static BLEScan* pBLEScan;
+
 static boolean doConnect = false;
 static boolean connected = false;
-static boolean doScan = false;
+static boolean doScan = true;
 
 static BLEAdvertisedDevice* myDevice;
 
 static BLERemoteCharacteristic* Check;
-static BLERemoteCharacteristic* Passwd;
-static BLERemoteCharacteristic* Admin;
-static BLERemoteCharacteristic* Action;
-static BLERemoteCharacteristic* Incoming;
 
 static boolean PassOK = false;
+static boolean PassOK0 = false;
+static boolean PassOK2 = false;
+static boolean PassOK3 = false;
+static boolean Yes = false;
+
 
 static void HODOR(){
   Serial.println("Door is opened");
+  delay(7000);
 }
 
 static void PasswdCheck(
@@ -44,45 +44,43 @@ static void PasswdCheck(
     Serial.println(length);
     Serial.print("data: ");
     Serial.println((char*)pData);
+    //
+    // Serial.print("Yes is: ");
+    // Serial.println(Yes);
+    // Serial.print("OK is: ");
+    // Serial.println(PassOK);
+    // Serial.print("OK2 is: ");
+    // Serial.println(PassOK2);
+    // Serial.print("OK3 is: ");
+    // Serial.println(PassOK3);
 
     char password[] = "060593";
-    boolean Yes = true;
 
+    if (PassOK0 == false){
+    Yes = true;
     Serial.println("Checking password");
     for (int i = 0; i < strlen(password); i++) {
       if(password[i] != (char)pData[i]){
         Yes = false;
         Serial.println("Password is not the same");
-        break;
+        ESP.restart();
       }
     }
+      PassOK0 = true;
+  }
+
+  if(PassOK2 == true){
+    Serial.println("Password is ok, go to action");
+    PassOK2 = false;
+    PassOK3 = true;
+  }
+
     if(Yes == true){
+      Serial.println("Password checked, it is ok");
+      Yes = false;
       PassOK = true;
-      Serial.println("Writing value to Incoming PORT");
-      String a = "1";
-      // uint8_t a[]={0x1};
-      Incoming->writeValue(a.c_str());
-      Serial.println("Written");
-    }
-}
-
-static void DoAction(
-  BLERemoteCharacteristic* pBLERemoteCharacteristic,
-  uint8_t* pData,
-  size_t length,
-  bool isNotify) {
-    Serial.print("Notify callback for characteristic ");
-    Serial.print("Action UUID");
-    Serial.print(" of data length ");
-    Serial.println(length);
-    Serial.print("data: ");
-    Serial.println((char*)pData);
-
-    if(PassOK == true){
-      PassOK = false;
-      HODOR();
-      Incoming->writeValue("1", 1);
-      pClient->disconnect();
+      PassOK2 = true;
+      Serial.println("Writing value");
     }
 }
 
@@ -98,6 +96,11 @@ class MyClientCallback : public BLEClientCallbacks {
 
 bool connectToServer() {
 
+    Serial.println("Forming a connection to ");
+
+    BLEClient*  pClient  = BLEDevice::createClient();
+
+    Serial.println(" - Created client");
 
     pClient->setClientCallbacks(new MyClientCallback());
 
@@ -111,41 +114,34 @@ bool connectToServer() {
     if (pRemoteService == nullptr) {
       Serial.print("Failed to find our service UUID: ");
       Serial.println(serviceUUID.toString().c_str());
-      pClient->disconnect();
+      ESP.restart();
     }
     Serial.println(" - Found our service");
 
 
     // Obtain a reference to the characteristic in the service of the remote BLE server.
     Check = pRemoteService->getCharacteristic(checkUUID);
-    Passwd = pRemoteService->getCharacteristic(passwdUUID);
-    Admin = pRemoteService->getCharacteristic(adminUUID);
-    Action = pRemoteService->getCharacteristic(actionUUID);
-    Incoming = pRemoteService->getCharacteristic(incomingUUID);
 
-    if (Check == nullptr || Passwd == nullptr || Action == nullptr || Incoming == nullptr) {
+    if (Check == nullptr) {
       Serial.print("Failed to find our characteristic UUID: ");
       Serial.println(checkUUID.toString().c_str());
-      pClient->disconnect();
-      return false;
+      ESP.restart();
     }
     Serial.println(" - Found our characteristic");
 
-    if(Passwd->canNotify()){
-      Serial.println("Registered notify dor Passwd");
-      Passwd->registerForNotify(PasswdCheck);}
-
-    // if(Admin->canNotify()){
-    //   Admin->registerForNotify(PasswdCheck);}
-
-    if(Action->canNotify()){
-      Serial.println("Registered notify dor Action");
-      Action->registerForNotify(DoAction);}
+    if(Check->canNotify()){
+      Serial.println("Registered notify for Passwd");
+      Check->registerForNotify(PasswdCheck);}
+      else{
+        ESP.restart();
+      }
 
     Serial.println("Writing value of MAC to server");
-    // Check->writeValue(BLEDevice::getAddress().toString().c_str(), BLEDevice::getAddress().toString().length());
-    Check->writeValue(BLEDevice::getAddress().toString().c_str());
+    delay(200);
+    Check->writeValue(BLEDevice::getAddress().toString().c_str(), BLEDevice::getAddress().toString().length());
+    // Check->writeValue(BLEDevice::getAddress().toString().c_str());
     Serial.println("Done");
+    doConnect = false;
 }
 
 
@@ -166,12 +162,12 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
           int RSSIL = advertisedDevice.getRSSI();
           Serial.print("RSSI is: ");
           Serial.println(RSSIL);
-          if (RSSIL >= -70) {
+          if (RSSIL >= -90) {
             Serial.println("Our device is near");
             BLEDevice::getScan()->stop();
             myDevice = new BLEAdvertisedDevice(advertisedDevice);
             doConnect = true;
-            doScan = true;
+            doScan = false;
             }
     } // Found our server
   } // onResult
@@ -182,22 +178,6 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Starting Arduino BLE Client application...");
   BLEDevice::init("");
-
-  Serial.println("Forming a connection to ");
-
-  BLEClient*  pClient  = BLEDevice::createClient();
-
-  Serial.println(" - Created client");
-
-  // Retrieve a Scanner and set the callback we want to use to be informed when we
-  // have detected a new device.  Specify that we want active scanning and start the
-  // scan to run for 1 second.
-  BLEScan* pBLEScan = BLEDevice::getScan();
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setActiveScan(true);
-  pBLEScan->start(1, false);
-
-
 } // End of setup.
 
 // This is the Arduino main loop function.
@@ -209,10 +189,31 @@ void loop() {
       Serial.println("We have failed to connect to the server; there is nothin more we will do.");
       ESP.restart();
     }
-    doConnect = false;
   }
-  //
-  // Serial.println("New scan: ");
-  // BLEDevice::getScan()->start(0);  // this is just eample to start scan after disconnect, most likely there is better way to do it in arduino
-  // delay(100);
+  if(PassOK == true){
+    String a = "1";
+    // uint8_t a[]={0x1};
+    Check->writeValue(a.c_str());
+    PassOK = false;
+    Serial.println("Written");
+  }
+  if(PassOK3 == true){
+    String a = "1";
+    // uint8_t a[]={0x1};
+    Check->writeValue(a.c_str());
+    Serial.println("Written");
+    HODOR();
+    ESP.restart();
+  }
+  if (doScan == true){
+    // Retrieve a Scanner and set the callback we want to use to be informed when we
+    // have detected a new device.  Specify that we want active scanning and start the
+    // scan to run for 1 second.
+    BLEScan* pBLEScan = BLEDevice::getScan();
+    pBLEScan->setInterval(1349);
+    pBLEScan->setWindow(449);
+    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+    pBLEScan->setActiveScan(true);
+    pBLEScan->start(3, false);
+  }
 } // End of loop
